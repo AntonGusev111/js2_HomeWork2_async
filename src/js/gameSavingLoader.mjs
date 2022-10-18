1a2cd0d1667d916ab889e4e3337fe93219c29f1f
GusevADresume <resumeGusevAD@gmail.com>
2022-10-19 02:25:28 +0300

    v1

---
+++ src/js/gameSavingLoader.mjs
@@ -0,0 +1,17 @@
+import json from './parser.mjs';
+import read from './reader.mjs';
+
+ class gameSavingLoader{
+    static load(){
+        const value =( async () =>{
+             await delay(500);
+             return 'value' 
+        })
+        return value
+    }
+}
+
+
+
+
+console.log(gameSavingLoader.load())
